edsonCVN/cacti-edson-fork

feat(satp): add support for NFTs
Signed-off-by: Tomás Silva <tomas.v.silva@tecnico.ulisboa.pt>

packages/cactus-plugin-satp-hermes/src/main/typescript/core/satp-utils.ts
@@ -1,5 +1,28 @@
+import { Asset } from "../cross-chain-mechanisms/bridge/ontology/assets/asset";
 import { MessageType } from "../generated/proto/cacti/satp/v02/common/message_pb";
 import { getEnumKeyByValue } from "../services/utils";
+import {
+  TokenIdMissingError,
+  LedgerAssetError,
+  AmountMissingError,
+  UniqueTokenDescriptorMissingError,
+} from "./errors/satp-service-errors";
+import { SATPLogger } from "./satp-logger";
+import { protoToAsset } from "./stage-services/service-utils";
+import { LedgerType } from "@hyperledger/cactus-core-api";
+import { NetworkId } from "../public-api";
+import { TokenType } from "../generated/proto/cacti/satp/v02/common/message_pb";
+import { SessionData } from "../generated/proto/cacti/satp/v02/session/session_pb";
+
+export interface SessionAssetBuildData {
+  token: Asset;
+  networkId: NetworkId;
+}
+
+export enum SessionSide {
+  CLIENT,
+  SERVER,
+}
 
 export function getMessageTypeName(
   messageType: MessageType | undefined,
@@ -10,4 +33,65 @@ export function getMessageTypeName(
       : getEnumKeyByValue(MessageType, MessageType.UNSPECIFIED)) ||
     "UNSPECIFIED"
   );
+}
+
+export function buildAndCheckAsset(
+  fnTag: string,
+  protocolStep: string,
+  logger: SATPLogger,
+  sessionData: SessionData,
+  sessionSide: SessionSide,
+): SessionAssetBuildData {
+  let sessionAsset: any;
+  if (sessionSide == SessionSide.CLIENT) {
+    if (sessionData.senderAsset == undefined) {
+      throw new LedgerAssetError(fnTag);
+    }
+    sessionAsset = sessionData.senderAsset;
+  } else {
+    if (sessionData.receiverAsset == undefined) {
+      throw new LedgerAssetError(fnTag);
+    }
+    sessionAsset = sessionData.receiverAsset;
+  }
+  const networkId = {
+    id: sessionAsset.networkId?.id,
+    ledgerType: sessionAsset.networkId?.type as LedgerType,
+  } as NetworkId;
+  let token: Asset;
+
+  switch (sessionAsset.tokenType) {
+    case TokenType.NONSTANDARD_FUNGIBLE:
+      token = protoToAsset(sessionAsset, networkId) as Asset;
+      if (token.id == undefined) {
+        throw new TokenIdMissingError(fnTag);
+      }
+      if (
+        !("amount" in token) ||
+        ("amount" in token && token.amount == undefined)
+      ) {
+        throw new AmountMissingError(fnTag);
+      }
+      logger.debug(
+        `${fnTag}, ${protocolStep} Asset ID: ${token.id} amount: ${token.amount}`,
+      );
+      return { token: token, networkId: networkId } as SessionAssetBuildData;
+    case TokenType.NONSTANDARD_NONFUNGIBLE:
+      token = protoToAsset(sessionAsset, networkId) as Asset;
+      if (token.id == undefined) {
+        throw new TokenIdMissingError(fnTag);
+      }
+      if (
+        !("uniqueDescriptor" in token) ||
+        ("uniqueDescriptor" in token && token.uniqueDescriptor == undefined)
+      ) {
+        throw new UniqueTokenDescriptorMissingError(fnTag);
+      }
+      logger.debug(
+        `${fnTag}, ${protocolStep} Asset ID: ${token.id} uniqueDescriptor: ${token.uniqueDescriptor}`,
+      );
+      return { token: token, networkId: networkId } as SessionAssetBuildData;
+    default:
+      throw new Error(`Unsupported asset type ${sessionAsset.tokenType}`);
+  }
 }

packages/cactus-plugin-satp-hermes/src/test/typescript/test-utils.ts
@@ -21,6 +21,7 @@ import { EventEmitter } from "events";
 import { ICrossChainMechanismsOptions } from "../../main/typescript/cross-chain-mechanisms/satp-cc-manager";
 import { createMigrationSource } from "../../main/typescript/database/knex-migration-source";
 import { ExtensionConfig } from "../../main/typescript/services/validation/config-validating-functions/validate-extensions";
+import { TokenType as TransactAssetType } from "../../main/typescript/generated/proto/cacti/satp/v02/common/message_pb";
 
 export { BesuTestEnvironment } from "./environments/besu-test-environment";
 export { EthereumTestEnvironment } from "./environments/ethereum-test-environment";
@@ -233,12 +234,32 @@ export function getTransactRequest(
   to: BesuTestEnvironment | EthereumTestEnvironment | FabricTestEnvironment,
   fromAmount: string,
   toAmount: string,
+  assetType?: TransactAssetType,
 ): TransactRequest {
-  return {
-    contextID,
-    sourceAsset: { ...from.defaultAsset, amount: fromAmount },
-    receiverAsset: { ...to.defaultAsset, amount: toAmount },
-  };
+  if (assetType === undefined) {
+    return {
+      contextID,
+      sourceAsset: { ...from.defaultAsset, amount: fromAmount },
+      receiverAsset: { ...to.defaultAsset, amount: toAmount },
+    };
+  } else {
+    switch (assetType) {
+      case TransactAssetType.NONSTANDARD_FUNGIBLE:
+        return {
+          contextID,
+          sourceAsset: { ...from.defaultAsset, amount: fromAmount },
+          receiverAsset: { ...to.defaultAsset, amount: toAmount },
+        };
+      case TransactAssetType.NONSTANDARD_NONFUNGIBLE:
+        return {
+          contextID,
+          sourceAsset: { ...from.nonFungibleDefaultAsset, amount: fromAmount },
+          receiverAsset: { ...to.nonFungibleDefaultAsset, amount: toAmount },
+        };
+      default:
+        throw new Error(`Unsupported asset type: ${assetType}`);
+    }
+  }
 }
 
 export interface PGDatabaseConfig {

packages/cactus-plugin-satp-hermes/src/test/typescript/unit/services.test.ts
@@ -314,7 +314,7 @@ describe("SATP Services Testing", () => {
 
     sessionData.senderAsset = create(AssetSchema, {
       tokenId: "MOCK_TOKEN_ID",
-      tokenType: TokenType.ERC20,
+      tokenType: TokenType.NONSTANDARD_FUNGIBLE,
       amount: BigInt(0),
       owner: "MOCK_SENDER_ASSET_OWNER",
       contractName: "MOCK_SENDER_ASSET_CONTRACT_NAME",
@@ -325,7 +325,7 @@ describe("SATP Services Testing", () => {
       }),
     });
     sessionData.receiverAsset = create(AssetSchema, {
-      tokenType: TokenType.ERC20,
+      tokenType: TokenType.NONSTANDARD_FUNGIBLE,
       amount: BigInt(0),
       owner: "MOCK_RECEIVER_ASSET_OWNER",
       contractName: "MOCK_RECEIVER_ASSET_CONTRACT_NAME",

packages/cactus-plugin-satp-hermes/src/test/typescript/unit/shutdown-state.test.ts
@@ -240,9 +240,10 @@ describe("Shutdown Verify State Tests", () => {
         id: "mockNetworkId",
         ledgerType: LedgerType.Ethereum,
       },
-      tokenType: "ERC20",
+      tokenType: "NONSTANDARD_FUNGIBLE",
       referenceId: "",
       amount: "100",
+      ercTokenStandard: "ERC20",
     };
 
     const transactRequest: TransactRequest = {

Tomas-Silva2187/cacti

packages/cactus-plugin-satp-hermes/src/main/typescript/core/satp-utils.ts
@@ -1,5 +1,28 @@
+import { Asset } from "../cross-chain-mechanisms/bridge/ontology/assets/asset";
 import { MessageType } from "../generated/proto/cacti/satp/v02/common/message_pb";
 import { getEnumKeyByValue } from "../services/utils";
+import {
+  TokenIdMissingError,
+  LedgerAssetError,
+  AmountMissingError,
+  UniqueTokenDescriptorMissingError,
+} from "./errors/satp-service-errors";
+import { SATPLogger } from "./satp-logger";
+import { protoToAsset } from "./stage-services/service-utils";
+import { LedgerType } from "@hyperledger/cactus-core-api";
+import { NetworkId } from "../public-api";
+import { TokenType } from "../generated/proto/cacti/satp/v02/common/message_pb";
+import { SessionData } from "../generated/proto/cacti/satp/v02/session/session_pb";
+
+export interface SessionAssetBuildData {
+  token: Asset;
+  networkId: NetworkId;
+}
+
+export enum SessionSide {
+  CLIENT,
+  SERVER,
+}
 
 export function getMessageTypeName(
   messageType: MessageType | undefined,
@@ -10,4 +33,65 @@ export function getMessageTypeName(
       : getEnumKeyByValue(MessageType, MessageType.UNSPECIFIED)) ||
     "UNSPECIFIED"
   );
+}
+
+export function buildAndCheckAsset(
+  fnTag: string,
+  protocolStep: string,
+  logger: SATPLogger,
+  sessionData: SessionData,
+  sessionSide: SessionSide,
+): SessionAssetBuildData {
+  let sessionAsset: any;
+  if (sessionSide == SessionSide.CLIENT) {
+    if (sessionData.senderAsset == undefined) {
+      throw new LedgerAssetError(fnTag);
+    }
+    sessionAsset = sessionData.senderAsset;
+  } else {
+    if (sessionData.receiverAsset == undefined) {
+      throw new LedgerAssetError(fnTag);
+    }
+    sessionAsset = sessionData.receiverAsset;
+  }
+  const networkId = {
+    id: sessionAsset.networkId?.id,
+    ledgerType: sessionAsset.networkId?.type as LedgerType,
+  } as NetworkId;
+  let token: Asset;
+
+  switch (sessionAsset.tokenType) {
+    case TokenType.FUNGIBLE:
+      token = protoToAsset(sessionAsset, networkId) as Asset;
+      if (token.id == undefined) {
+        throw new TokenIdMissingError(fnTag);
+      }
+      if (
+        !("amount" in token) ||
+        ("amount" in token && token.amount == undefined)
+      ) {
+        throw new AmountMissingError(fnTag);
+      }
+      logger.debug(
+        `${fnTag}, ${protocolStep} Asset ID: ${token.id} amount: ${token.amount}`,
+      );
+      return { token: token, networkId: networkId } as SessionAssetBuildData;
+    case TokenType.NONFUNGIBLE:
+      token = protoToAsset(sessionAsset, networkId) as Asset;
+      if (token.id == undefined) {
+        throw new TokenIdMissingError(fnTag);
+      }
+      if (
+        !("uniqueDescriptor" in token) ||
+        ("uniqueDescriptor" in token && token.uniqueDescriptor == undefined)
+      ) {
+        throw new UniqueTokenDescriptorMissingError(fnTag);
+      }
+      logger.debug(
+        `${fnTag}, ${protocolStep} Asset ID: ${token.id} uniqueDescriptor: ${token.uniqueDescriptor}`,
+      );
+      return { token: token, networkId: networkId } as SessionAssetBuildData;
+    default:
+      throw new Error(`Unsupported asset type ${sessionAsset.tokenType}`);
+  }
 }

packages/cactus-plugin-satp-hermes/src/test/typescript/test-utils.ts
@@ -20,6 +20,7 @@ import { Containers } from "@hyperledger/cactus-test-tooling/src/main/typescript
 import { EventEmitter } from "events";
 import { ICrossChainMechanismsOptions } from "../../main/typescript/cross-chain-mechanisms/satp-cc-manager";
 import { createMigrationSource } from "../../main/typescript/database/knex-migration-source";
+import { TokenType as TransactAssetType } from "../../main/typescript/generated/proto/cacti/satp/v02/common/message_pb";
 
 export { BesuTestEnvironment } from "./environments/besu-test-environment";
 export { EthereumTestEnvironment } from "./environments/ethereum-test-environment";
@@ -233,12 +234,32 @@ export function getTransactRequest(
   to: BesuTestEnvironment | EthereumTestEnvironment | FabricTestEnvironment,
   fromAmount: string,
   toAmount: string,
+  assetType?: TransactAssetType,
 ): TransactRequest {
-  return {
-    contextID,
-    sourceAsset: { ...from.defaultAsset, amount: fromAmount },
-    receiverAsset: { ...to.defaultAsset, amount: toAmount },
-  };
+  if (assetType === undefined) {
+    return {
+      contextID,
+      sourceAsset: { ...from.defaultAsset, amount: fromAmount },
+      receiverAsset: { ...to.defaultAsset, amount: toAmount },
+    };
+  } else {
+    switch (assetType) {
+      case TransactAssetType.FUNGIBLE:
+        return {
+          contextID,
+          sourceAsset: { ...from.defaultAsset, amount: fromAmount },
+          receiverAsset: { ...to.defaultAsset, amount: toAmount },
+        };
+      case TransactAssetType.NONFUNGIBLE:
+        return {
+          contextID,
+          sourceAsset: { ...from.nonFungibleDefaultAsset, amount: fromAmount },
+          receiverAsset: { ...to.nonFungibleDefaultAsset, amount: toAmount },
+        };
+      default:
+        throw new Error(`Unsupported asset type: ${assetType}`);
+    }
+  }
 }
 
 export interface PGDatabaseConfig {

packages/cactus-plugin-satp-hermes/src/test/typescript/unit/services.test.ts
@@ -314,7 +314,7 @@ describe("SATP Services Testing", () => {
 
     sessionData.senderAsset = create(AssetSchema, {
       tokenId: "MOCK_TOKEN_ID",
-      tokenType: TokenType.ERC20,
+      tokenType: TokenType.FUNGIBLE,
       amount: BigInt(0),
       owner: "MOCK_SENDER_ASSET_OWNER",
       contractName: "MOCK_SENDER_ASSET_CONTRACT_NAME",
@@ -325,7 +325,7 @@ describe("SATP Services Testing", () => {
       }),
     });
     sessionData.receiverAsset = create(AssetSchema, {
-      tokenType: TokenType.ERC20,
+      tokenType: TokenType.FUNGIBLE,
       amount: BigInt(0),
       owner: "MOCK_RECEIVER_ASSET_OWNER",
       contractName: "MOCK_RECEIVER_ASSET_CONTRACT_NAME",

packages/cactus-plugin-satp-hermes/src/test/typescript/unit/shutdown-state.test.ts
@@ -240,9 +240,10 @@ describe("Shutdown Verify State Tests", () => {
         id: "mockNetworkId",
         ledgerType: LedgerType.Ethereum,
       },
-      tokenType: "ERC20",
+      tokenType: "FUNGIBLE",
       referenceId: "",
       amount: "100",
+      ercTokenStandard: "ERC20",
     };
 
     const transactRequest: TransactRequest = {
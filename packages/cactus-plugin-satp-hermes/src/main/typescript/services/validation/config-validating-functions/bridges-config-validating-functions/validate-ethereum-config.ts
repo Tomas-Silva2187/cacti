edsonCVN/cacti-edson-fork

feat(satp-hermes): improve satp gateway configuration object
Signed-off-by: Tomas Silva <tomas.v.silva@tecnico.ulisboa.pt>
Signed-off-by: Tomás Silva <tomas.v.silva@tecnico.ulisboa.pt>

packages/cactus-plugin-satp-hermes/src/main/typescript/services/validation/config-validating-functions/bridges-config-validating-functions/validate-ethereum-config.ts
@@ -1,5 +1,4 @@
 import {
-  GasTransactionConfig,
   type Web3SigningCredential,
   type Web3SigningCredentialCactiKeychainRef,
   type Web3SigningCredentialGethKeychainPassword,
@@ -7,14 +6,9 @@ import {
   type Web3SigningCredentialPrivateKeyHex,
   Web3SigningCredentialType,
 } from "@hyperledger/cactus-plugin-ledger-connector-ethereum";
-import {
-  type EthereumOptionsJSON,
-  isEthereumOptionsJSON,
-} from "./validate-ethereum-options";
+import { isEthereumOptionsJSON } from "./validate-ethereum-options";
 import { isClaimFormat } from "./validate-bungee-options";
-import type { ClaimFormat } from "../../../../generated/proto/cacti/satp/v02/common/message_pb";
-import { NetworkId, NetworkOptionsJSON } from "../validate-cc-config";
-import { KeyPairJSON } from "../validate-key-pair-json";
+import { isGasConfig } from "../validate-cc-config";
 import { isNetworkId } from "../validate-satp-gateway-identity";
 import { Logger } from "@hyperledger/cactus-common";
 import {
@@ -22,17 +16,8 @@ import {
   identifyAndCheckConfigFormat,
 } from "../../../utils";
 import { LedgerType } from "@hyperledger/cactus-core-api";
-
-export interface EthereumConfigJSON extends NetworkOptionsJSON {
-  signingCredential: Web3SigningCredential;
-  connectorOptions: EthereumOptionsJSON;
-  wrapperContractName?: string;
-  wrapperContractAddress?: string;
-  gasConfig?: GasTransactionConfig;
-  leafId?: string;
-  keyPair?: KeyPairJSON;
-  claimFormats?: ClaimFormat[];
-}
+import { IEthereumNetworkConfig } from "../../../../cross-chain-mechanisms/bridge/bridge-types";
+import { NetworkId } from "../../../../public-api";
 
 // Type guard for Web3SigningCredentialType
 export function isWeb3SigningCredentialType(
@@ -56,8 +41,8 @@ function isWeb3SigningCredentialCactiKeychainRef(
   return (
     typeof obj === "object" &&
     obj !== null &&
-    "ethAccount" in obj &&
-    typeof objRecord.ethAccount === "string" &&
+    "transactionSignerEthAccount" in obj &&
+    typeof objRecord.transactionSignerEthAccount === "string" &&
     "keychainEntryKey" in obj &&
     typeof objRecord.keychainEntryKey === "string" &&
     (!("keychainId" in obj) || typeof objRecord.keychainId === "string") &&
@@ -74,8 +59,8 @@ function isWeb3SigningCredentialGethKeychainPassword(
   return (
     typeof obj === "object" &&
     obj !== null &&
-    "ethAccount" in obj &&
-    typeof objRecord.ethAccount === "string" &&
+    "transactionSignerEthAccount" in obj &&
+    typeof objRecord.transactionSignerEthAccount === "string" &&
     "secret" in obj &&
     typeof objRecord.secret === "string" &&
     "type" in obj &&
@@ -91,8 +76,8 @@ function isWeb3SigningCredentialPrivateKeyHex(
   return (
     typeof obj === "object" &&
     obj !== null &&
-    "ethAccount" in obj &&
-    typeof objRecord.ethAccount === "string" &&
+    "transactionSignerEthAccount" in obj &&
+    typeof objRecord.transactionSignerEthAccount === "string" &&
     "secret" in obj &&
     typeof objRecord.secret === "string" &&
     "type" in obj &&
@@ -130,30 +115,6 @@ function isWeb3SigningCredential(
   );
 }
 
-function isGasConfig(obj: unknown, log: Logger): obj is GasTransactionConfig {
-  if (!obj || typeof obj !== "object") {
-    log.error("isGasConfig: obj null or not obj");
-    throw new TypeError(
-      "isGasConfig: obj null or not obj" + JSON.stringify(obj),
-    );
-  }
-
-  const objRecord = obj as Record<string, unknown>;
-
-  return (
-    ("gas" in obj &&
-      typeof objRecord.gas === "string" &&
-      "gasPrice" in obj &&
-      typeof objRecord.gasPrice === "string") ||
-    ("gasLimit" in obj &&
-      typeof objRecord.gasLimit === "string" &&
-      "maxFeePerGas" in obj &&
-      typeof objRecord.maxFeePerGas === "string" &&
-      "maxPriorityFeePerGas" in obj &&
-      typeof objRecord.maxPriorityFeePerGas === "string")
-  );
-}
-
 export function isEthereumNetworkId(obj: NetworkId) {
   return (obj.ledgerType as LedgerType) === LedgerType.Ethereum;
 }
@@ -162,7 +123,7 @@ export function isEthereumNetworkId(obj: NetworkId) {
 export function isEthereumConfigJSON(
   obj: unknown,
   log: Logger,
-): obj is EthereumConfigJSON {
+): obj is IEthereumNetworkConfig {
   if (typeof obj !== "object" || obj === null) {
     log.error("isEthereumConfigJSON: obj is not an object or is null");
     return false;

packages/cactus-plugin-satp-hermes/src/main/typescript/services/validation/config-validating-functions/validate-cc-config.ts
@@ -9,32 +9,24 @@ import {
   isBesuConfigJSON,
   isBesuNetworkId,
 } from "./bridges-config-validating-functions/validate-besu-config";
-import { createBesuOptions } from "./bridges-config-validating-functions/validate-besu-options";
 import {
   isEthereumConfigJSON,
   isEthereumNetworkId,
 } from "./bridges-config-validating-functions/validate-ethereum-config";
-import { createEthereumOptions } from "./bridges-config-validating-functions/validate-ethereum-options";
 import { ICrossChainMechanismsOptions } from "../../../cross-chain-mechanisms/satp-cc-manager";
-import { INetworkOptions } from "../../../cross-chain-mechanisms/bridge/bridge-types";
+import {
+  IBesuNetworkConfig,
+  IEthereumNetworkConfig,
+  INetworkOptions,
+} from "../../../cross-chain-mechanisms/bridge/bridge-types";
 import { IFabricLeafNeworkOptions } from "../../../cross-chain-mechanisms/bridge/leafs/fabric-leaf";
-import { IBesuLeafNeworkOptions } from "../../../cross-chain-mechanisms/bridge/leafs/besu-leaf";
-import { IEthereumLeafNeworkOptions } from "../../../cross-chain-mechanisms/bridge/leafs/ethereum-leaf";
 import {
   DeploymentTargetOrganization,
   FileBase64,
 } from "@hyperledger/cactus-plugin-ledger-connector-fabric";
 import path from "path";
 import { Logger } from "@hyperledger/cactus-common";
-
-export interface NetworkOptionsJSON {
-  networkIdentification: NetworkId;
-}
-
-export interface NetworkId {
-  id: string;
-  ledgerType: string;
-}
+import { NetworkId } from "../../../public-api";
 
 function isNetworkId(obj: unknown): obj is NetworkId {
   return (
@@ -47,11 +39,35 @@ function isNetworkId(obj: unknown): obj is NetworkId {
   );
 }
 
+export function isGasConfig(obj: unknown, log: Logger) {
+  if (!obj || typeof obj !== "object") {
+    log.error("isGasConfig: obj null or not obj");
+    throw new TypeError(
+      "isGasConfig: obj null or not obj" + JSON.stringify(obj),
+    );
+  }
+
+  const objRecord = obj as Record<string, unknown>;
+
+  return (
+    ("gas" in obj &&
+      typeof objRecord.gas === "string" &&
+      "gasPrice" in obj &&
+      typeof objRecord.gasPrice === "string") ||
+    ("gasLimit" in obj &&
+      typeof objRecord.gasLimit === "string" &&
+      "maxFeePerGas" in obj &&
+      typeof objRecord.maxFeePerGas === "string" &&
+      "maxPriorityFeePerGas" in obj &&
+      typeof objRecord.maxPriorityFeePerGas === "string") ||
+    (Object.keys(objRecord).length === 1 &&
+      Object.keys(objRecord)[0] === "gasLimit" &&
+      typeof objRecord.gasLimit === "string")
+  );
+}
+
 // Type guard for NetworkConfigJSON
-function NetworkOptionsJSON(
-  obj: unknown,
-  log: Logger,
-): obj is NetworkOptionsJSON {
+function NetworkOptionsJSON(obj: unknown, log: Logger): obj is INetworkOptions {
   if (typeof obj !== "object" || obj === null) {
     return false;
   }
@@ -71,7 +87,7 @@ function NetworkOptionsJSON(
 function NetworkOptionsJSONArray(
   obj: unknown,
   log: Logger,
-): obj is NetworkOptionsJSON[] {
+): obj is INetworkOptions[] {
   return (
     Array.isArray(obj) && obj.every((item) => NetworkOptionsJSON(item, log))
   );
@@ -92,7 +108,7 @@ function isCCConfigJSON(
 }
 
 async function createBridgeConfig(
-  configs: NetworkOptionsJSON[] | undefined = [],
+  configs: INetworkOptions[] | undefined = [],
   log: Logger,
 ): Promise<INetworkOptions[]> {
   const bridgesConfigParsed: INetworkOptions[] = [];
@@ -239,49 +255,37 @@ async function createBridgeConfig(
       isBesuNetworkId(config.networkIdentification) &&
       isBesuConfigJSON(config, log)
     ) {
-      const besuOptions = createBesuOptions(config.connectorOptions);
+      //const besuOptions = createBesuOptions(config.connectorOptions);
       const besuConfig = {
         networkIdentification: config.networkIdentification,
         signingCredential: config.signingCredential,
-        connectorOptions: besuOptions,
+        connectorOptions: config.connectorOptions,
         leafId: config.leafId,
-        keyPair:
-          config.keyPair === undefined
-            ? undefined
-            : {
-                publicKey: Buffer.from(config.keyPair.publicKey, "hex"),
-                privateKey: Buffer.from(config.keyPair.privateKey, "hex"),
-              },
+        keyPair: config.keyPair,
         claimFormats: config.claimFormats,
         wrapperContractAddress: config.wrapperContractAddress,
         wrapperContractName: config.wrapperContractName,
-        gas: config.gas,
-      } as Partial<IBesuLeafNeworkOptions> & INetworkOptions;
+        gasConfig: config.gasConfig,
+      } as Partial<IBesuNetworkConfig> & INetworkOptions;
 
       bridgesConfigParsed.push(besuConfig);
     } else if (
       isEthereumNetworkId(config.networkIdentification) &&
       isEthereumConfigJSON(config, log)
     ) {
-      const ethereumOptions = createEthereumOptions(config.connectorOptions);
+      //const ethereumOptions = createEthereumOptions(config.connectorOptions);
 
       const ethereumConfig = {
         networkIdentification: config.networkIdentification,
         signingCredential: config.signingCredential,
-        connectorOptions: ethereumOptions,
+        connectorOptions: config.connectorOptions,
         leafId: config.leafId,
-        keyPair:
-          config.keyPair === undefined
-            ? undefined
-            : {
-                publicKey: Buffer.from(config.keyPair.publicKey, "hex"),
-                privateKey: Buffer.from(config.keyPair.privateKey, "hex"),
-              },
+        keyPair: config.keyPair,
         claimFormats: config.claimFormats,
         wrapperContractAddress: config.wrapperContractAddress,
         wrapperContractName: config.wrapperContractName,
         gasConfig: config.gasConfig,
-      } as Partial<IEthereumLeafNeworkOptions> & INetworkOptions;
+      } as Partial<IEthereumNetworkConfig> & INetworkOptions;
 
       bridgesConfigParsed.push(ethereumConfig);
     }

Tomas-Silva2187/cacti

packages/cactus-plugin-satp-hermes/src/main/typescript/services/validation/config-validating-functions/bridges-config-validating-functions/validate-ethereum-config.ts
@@ -1,5 +1,4 @@
 import {
-  GasTransactionConfig,
   type Web3SigningCredential,
   type Web3SigningCredentialCactiKeychainRef,
   type Web3SigningCredentialGethKeychainPassword,
@@ -7,14 +6,9 @@ import {
   type Web3SigningCredentialPrivateKeyHex,
   Web3SigningCredentialType,
 } from "@hyperledger/cactus-plugin-ledger-connector-ethereum";
-import {
-  type EthereumOptionsJSON,
-  isEthereumOptionsJSON,
-} from "./validate-ethereum-options";
+import { isEthereumOptionsJSON } from "./validate-ethereum-options";
 import { isClaimFormat } from "./validate-bungee-options";
-import type { ClaimFormat } from "../../../../generated/proto/cacti/satp/v02/common/message_pb";
-import { NetworkId, NetworkOptionsJSON } from "../validate-cc-config";
-import { KeyPairJSON } from "../validate-key-pair-json";
+import { isGasConfig } from "../validate-cc-config";
 import { isNetworkId } from "../validate-satp-gateway-identity";
 import { Logger } from "@hyperledger/cactus-common";
 import {
@@ -22,17 +16,8 @@ import {
   identifyAndCheckConfigFormat,
 } from "../../../utils";
 import { LedgerType } from "@hyperledger/cactus-core-api";
-
-export interface EthereumConfigJSON extends NetworkOptionsJSON {
-  signingCredential: Web3SigningCredential;
-  connectorOptions: EthereumOptionsJSON;
-  wrapperContractName?: string;
-  wrapperContractAddress?: string;
-  gasConfig?: GasTransactionConfig;
-  leafId?: string;
-  keyPair?: KeyPairJSON;
-  claimFormats?: ClaimFormat[];
-}
+import { IEthereumNetworkConfig } from "../../../../cross-chain-mechanisms/bridge/bridge-types";
+import { NetworkId } from "../../../../public-api";
 
 // Type guard for Web3SigningCredentialType
 export function isWeb3SigningCredentialType(
@@ -56,8 +41,8 @@ function isWeb3SigningCredentialCactiKeychainRef(
   return (
     typeof obj === "object" &&
     obj !== null &&
-    "ethAccount" in obj &&
-    typeof objRecord.ethAccount === "string" &&
+    "bridgeOwnerAddress" in obj &&
+    typeof objRecord.bridgeOwnerAddress === "string" &&
     "keychainEntryKey" in obj &&
     typeof objRecord.keychainEntryKey === "string" &&
     (!("keychainId" in obj) || typeof objRecord.keychainId === "string") &&
@@ -74,8 +59,8 @@ function isWeb3SigningCredentialGethKeychainPassword(
   return (
     typeof obj === "object" &&
     obj !== null &&
-    "ethAccount" in obj &&
-    typeof objRecord.ethAccount === "string" &&
+    "bridgeOwnerAddress" in obj &&
+    typeof objRecord.bridgeOwnerAddress === "string" &&
     "secret" in obj &&
     typeof objRecord.secret === "string" &&
     "type" in obj &&
@@ -91,8 +76,8 @@ function isWeb3SigningCredentialPrivateKeyHex(
   return (
     typeof obj === "object" &&
     obj !== null &&
-    "ethAccount" in obj &&
-    typeof objRecord.ethAccount === "string" &&
+    "bridgeOwnerAddress" in obj &&
+    typeof objRecord.bridgeOwnerAddress === "string" &&
     "secret" in obj &&
     typeof objRecord.secret === "string" &&
     "type" in obj &&
@@ -130,30 +115,6 @@ function isWeb3SigningCredential(
   );
 }
 
-function isGasConfig(obj: unknown, log: Logger): obj is GasTransactionConfig {
-  if (!obj || typeof obj !== "object") {
-    log.error("isGasConfig: obj null or not obj");
-    throw new TypeError(
-      "isGasConfig: obj null or not obj" + JSON.stringify(obj),
-    );
-  }
-
-  const objRecord = obj as Record<string, unknown>;
-
-  return (
-    ("gas" in obj &&
-      typeof objRecord.gas === "string" &&
-      "gasPrice" in obj &&
-      typeof objRecord.gasPrice === "string") ||
-    ("gasLimit" in obj &&
-      typeof objRecord.gasLimit === "string" &&
-      "maxFeePerGas" in obj &&
-      typeof objRecord.maxFeePerGas === "string" &&
-      "maxPriorityFeePerGas" in obj &&
-      typeof objRecord.maxPriorityFeePerGas === "string")
-  );
-}
-
 export function isEthereumNetworkId(obj: NetworkId) {
   return (obj.ledgerType as LedgerType) === LedgerType.Ethereum;
 }
@@ -162,7 +123,7 @@ export function isEthereumNetworkId(obj: NetworkId) {
 export function isEthereumConfigJSON(
   obj: unknown,
   log: Logger,
-): obj is EthereumConfigJSON {
+): obj is IEthereumNetworkConfig {
   if (typeof obj !== "object" || obj === null) {
     log.error("isEthereumConfigJSON: obj is not an object or is null");
     return false;

packages/cactus-plugin-satp-hermes/src/main/typescript/services/validation/config-validating-functions/validate-cc-config.ts
@@ -9,32 +9,24 @@ import {
   isBesuConfigJSON,
   isBesuNetworkId,
 } from "./bridges-config-validating-functions/validate-besu-config";
-import { createBesuOptions } from "./bridges-config-validating-functions/validate-besu-options";
 import {
   isEthereumConfigJSON,
   isEthereumNetworkId,
 } from "./bridges-config-validating-functions/validate-ethereum-config";
-import { createEthereumOptions } from "./bridges-config-validating-functions/validate-ethereum-options";
 import { ICrossChainMechanismsOptions } from "../../../cross-chain-mechanisms/satp-cc-manager";
-import { INetworkOptions } from "../../../cross-chain-mechanisms/bridge/bridge-types";
+import {
+  IBesuNetworkConfig,
+  IEthereumNetworkConfig,
+  INetworkOptions,
+} from "../../../cross-chain-mechanisms/bridge/bridge-types";
 import { IFabricLeafNeworkOptions } from "../../../cross-chain-mechanisms/bridge/leafs/fabric-leaf";
-import { IBesuLeafNeworkOptions } from "../../../cross-chain-mechanisms/bridge/leafs/besu-leaf";
-import { IEthereumLeafNeworkOptions } from "../../../cross-chain-mechanisms/bridge/leafs/ethereum-leaf";
 import {
   DeploymentTargetOrganization,
   FileBase64,
 } from "@hyperledger/cactus-plugin-ledger-connector-fabric";
 import path from "path";
 import { Logger } from "@hyperledger/cactus-common";
-
-export interface NetworkOptionsJSON {
-  networkIdentification: NetworkId;
-}
-
-export interface NetworkId {
-  id: string;
-  ledgerType: string;
-}
+import { NetworkId } from "../../../public-api";
 
 function isNetworkId(obj: unknown): obj is NetworkId {
   return (
@@ -47,11 +39,32 @@ function isNetworkId(obj: unknown): obj is NetworkId {
   );
 }
 
+export function isGasConfig(obj: unknown, log: Logger) {
+  if (!obj || typeof obj !== "object") {
+    log.error("isGasConfig: obj null or not obj");
+    throw new TypeError(
+      "isGasConfig: obj null or not obj" + JSON.stringify(obj),
+    );
+  }
+
+  const objRecord = obj as Record<string, unknown>;
+
+  return (
+    ("gas" in obj &&
+      typeof objRecord.gas === "string" &&
+      "gasPrice" in obj &&
+      typeof objRecord.gasPrice === "string") ||
+    ("gasLimit" in obj &&
+      typeof objRecord.gasLimit === "string" &&
+      "maxFeePerGas" in obj &&
+      typeof objRecord.maxFeePerGas === "string" &&
+      "maxPriorityFeePerGas" in obj &&
+      typeof objRecord.maxPriorityFeePerGas === "string")
+  );
+}
+
 // Type guard for NetworkConfigJSON
-function NetworkOptionsJSON(
-  obj: unknown,
-  log: Logger,
-): obj is NetworkOptionsJSON {
+function NetworkOptionsJSON(obj: unknown, log: Logger): obj is INetworkOptions {
   if (typeof obj !== "object" || obj === null) {
     return false;
   }
@@ -71,7 +84,7 @@ function NetworkOptionsJSON(
 function NetworkOptionsJSONArray(
   obj: unknown,
   log: Logger,
-): obj is NetworkOptionsJSON[] {
+): obj is INetworkOptions[] {
   return (
     Array.isArray(obj) && obj.every((item) => NetworkOptionsJSON(item, log))
   );
@@ -92,7 +105,7 @@ function isCCConfigJSON(
 }
 
 async function createBridgeConfig(
-  configs: NetworkOptionsJSON[] | undefined = [],
+  configs: INetworkOptions[] | undefined = [],
   log: Logger,
 ): Promise<INetworkOptions[]> {
   const bridgesConfigParsed: INetworkOptions[] = [];
@@ -239,49 +252,37 @@ async function createBridgeConfig(
       isBesuNetworkId(config.networkIdentification) &&
       isBesuConfigJSON(config, log)
     ) {
-      const besuOptions = createBesuOptions(config.connectorOptions);
+      //const besuOptions = createBesuOptions(config.connectorOptions);
       const besuConfig = {
         networkIdentification: config.networkIdentification,
         signingCredential: config.signingCredential,
-        connectorOptions: besuOptions,
+        connectorOptions: config.connectorOptions,
         leafId: config.leafId,
-        keyPair:
-          config.keyPair === undefined
-            ? undefined
-            : {
-                publicKey: Buffer.from(config.keyPair.publicKey, "hex"),
-                privateKey: Buffer.from(config.keyPair.privateKey, "hex"),
-              },
+        keyPair: config.keyPair,
         claimFormats: config.claimFormats,
         wrapperContractAddress: config.wrapperContractAddress,
         wrapperContractName: config.wrapperContractName,
-        gas: config.gas,
-      } as Partial<IBesuLeafNeworkOptions> & INetworkOptions;
+        gasConfig: config.gasConfig,
+      } as Partial<IBesuNetworkConfig> & INetworkOptions;
 
       bridgesConfigParsed.push(besuConfig);
     } else if (
       isEthereumNetworkId(config.networkIdentification) &&
       isEthereumConfigJSON(config, log)
     ) {
-      const ethereumOptions = createEthereumOptions(config.connectorOptions);
+      //const ethereumOptions = createEthereumOptions(config.connectorOptions);
 
       const ethereumConfig = {
         networkIdentification: config.networkIdentification,
         signingCredential: config.signingCredential,
-        connectorOptions: ethereumOptions,
+        connectorOptions: config.connectorOptions,
         leafId: config.leafId,
-        keyPair:
-          config.keyPair === undefined
-            ? undefined
-            : {
-                publicKey: Buffer.from(config.keyPair.publicKey, "hex"),
-                privateKey: Buffer.from(config.keyPair.privateKey, "hex"),
-              },
+        keyPair: config.keyPair,
         claimFormats: config.claimFormats,
         wrapperContractAddress: config.wrapperContractAddress,
         wrapperContractName: config.wrapperContractName,
         gasConfig: config.gasConfig,
-      } as Partial<IEthereumLeafNeworkOptions> & INetworkOptions;
+      } as Partial<IEthereumNetworkConfig> & INetworkOptions;
 
       bridgesConfigParsed.push(ethereumConfig);
     }